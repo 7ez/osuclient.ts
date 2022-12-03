965038d1678bd86c94e820774e5d140158a64db9
Aochi <aochi@fuquila.net>
2022-12-03 11:07:01 +0100

    add: index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const client = require('./client') // i have no idea how to use import with this lol
+const constants = require('./constants')
+const packets = require('./packets')
+
+module.exports = {
+  client,
+  constants,
+  packets
+}
